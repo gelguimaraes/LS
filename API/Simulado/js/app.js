715ebfe0aa0c292b4ae34224e3aeb3485df02cff
Gel Guimarães <gesoaldo.guimaraes@academico.ifpb.edu.br>
2018-02-01 11:56:58 -0300

    Add files via upload

--- API/Simulado/js/app.js
+++ API/Simulado/js/app.js
@@ -8,34 +8,34 @@ const boxInfoCompare = document.querySelector('#boxInfoCompare')
 
 let address1 = url1Input.value.split("https://github.com/")[1]
 let address2 = url2Input.value.split("https://github.com/")[1]
-//console.log(address1)
-//console.log(address2)
+	//console.log(address1)
+	//console.log(address2)
 
 const headProject = `<div id="project"> <h2 class="title">Project</h2></div>`
 const headCommunity = `<div id="community"> <h2 class="title">Community</h2></div>`
 const headCode = `<div id="code"><h2 class="title">Code</h2</div>`
-	
+
 
 getCompare = (gitUser) => {
 
-const url = `https://api.github.com/repos/${gitUser}`
-	
-fetch(url)
-  .then(res => res.json())
-  .then(json => {
-	
-	const htmlContentProject = `<div class="content">
+	const url = `https://api.github.com/repos/${gitUser}`
+
+	fetch(url)
+		.then(res => res.json())
+		.then(json => {
+
+			const htmlContentProject = `<div class="content">
         <a href="${json.html_url}" target="_blank" class="subTitle">${json.name}</a>
         <p class="textContnt">
 			<i class="fa fa-user"></i>
 			<a href="${json.owner.url}" target="_blank">${json.owner.login}</a>
 		</p>
-        <p class="text"><i class="fa fa-circle-o"></i>${json.created_at}</p>
-        <p class="text"><i class="fa fa-refresh"></i>${json.updated_at}</p>
+        <p class="text"><i class="fa fa-circle-o"></i>${dateFns.format(new Date(json.created_at), 'ddd Mo, YYYY')}</p>
+        <p class="text"><i class="fa fa-refresh"></i>${dateFns.distanceInWords(json.created_at, json.updated_at)}</p>
       </div>`
-	  
-	  
-	  let htmlContentCommunity = `<div class="content">
+
+
+			const htmlContentCommunity = `<div class="content">
         <p class="text">
 			<i class="fa fa-star"></i>
 			<a href="${json.stargazers_url}" target="_blank">${json.stargazers_count}</a> 
@@ -44,72 +44,85 @@ fetch(url)
 		</p>
         <p class="text">
 			<i class="fa fa-users"></i>
-			<a href="${json.contributors_url}" target="_blank">${json.open_issues_count}</a>				
+			<a href="${json.contributors_url}" target="_blank" id="contr${json.name}"></a>				
 			<span> contributors</span>
 		</p>
       </div>`
-	  
-	  	let htmlContentCode = `<div class="content">
+
+			const htmlContentCode = `<div class="content">
         <p class="text">
-			<a href="${json.commits_url}" target="_blank" id="commit${json.name}"></a> 	
+			<a href="${json.commits_url.split("{/sha}")[0]}" target="_blank" id="commit${json.name}"></a> 	
 			<span>commits</span>
 		</p>
         <p class="textPercent" id="lang${json.name}"><i class="fa fa-language"></i></p>
-      </div>` 
-	  
-	  
-	const divProject = document.querySelector('#project')
-	const divCommunit = document.querySelector('#community')
-	const divCode = document.querySelector('#code')
-	
-	
-    divProject.insertAdjacentHTML('beforeend', htmlContentProject)
-	divCommunit.insertAdjacentHTML('beforeend', htmlContentCommunity)
-	divCode.insertAdjacentHTML('beforeend', htmlContentCode)
-	
-	
-	let textCommits = document.querySelector('#commit' + json.name)
-	getCommits(json.commits_url.split("{/sha}")[0], textCommits)
-
-	let textLanguages = document.querySelector('#lang' + json.name)
-	getLanguages(json.languages_url, textLanguages)
- 
-  })
-}
+      </div>`
 
 
-getLanguages = (url, seletor) =>{
-	fetch(url)
-	  .then(res => res.json())
-	  .then(json => {
-		  	let array = Object.entries(json);
-		 	let total = array.reduce((sum, item) => (sum + item[1]), 0);
+			const divProject = document.querySelector('#project')
+			const divCommunit = document.querySelector('#community')
+			const divCode = document.querySelector('#code')
+
+
+			divProject.insertAdjacentHTML('beforeend', htmlContentProject)
+			divCommunit.insertAdjacentHTML('beforeend', htmlContentCommunity)
+			divCode.insertAdjacentHTML('beforeend', htmlContentCode)
+
 			
-			//console.log(total)
-		 	let list = array.map((item) =>  "<p><span>" + item[0] + "</span>: " + (parseInt((item[1]/total) *100 )) +"%</p>").join(" ") ;
-		 	
-			seletor.insertAdjacentHTML('beforeend', list);
-		 })
+			let textContributors = document.querySelector('#contr' + json.name)
+			getContributors(json.contributors_url, textContributors)
+			
+			let textCommits = document.querySelector('#commit' + json.name)
+			getCommits(json.commits_url.split("{/sha}")[0], textCommits)
+
+			let textLanguages = document.querySelector('#lang' + json.name)
+			getLanguages(json.languages_url, textLanguages)
+
+		})
 }
-	
+
+
+getLanguages = (url, seletor) => {
+	fetch(url)
+		.then(res => res.json())
+		.then(json => {
+			let array = Object.entries(json);
+			let total = array.reduce((sum, item) => (sum + item[1]), 0);
+
+			//console.log(total)
+			let list = array.map((item) => "<p><span>" + item[0] + "</span>: " + ((item[1] / total) * 100).toFixed(2) + "%</p>").join(" ");
+
+			seletor.insertAdjacentHTML('beforeend', list);
+		})
+}
+
 getCommits = (url, seletor) => {
 	fetch(url)
-	  .then(res => res.json())
-	  .then(json => {
-		 let commits = json.length
-		 console.log(commits) 
-		 
-		 seletor.insertAdjacentHTML('beforeend', commits);
-	})
-}	
+		.then(res => res.json())
+		.then(json => {
+			let commits = json.length
+				//console.log(commits) 
 
-button.addEventListener('click', function(event) {
-	
+			seletor.insertAdjacentHTML('beforeend', commits);
+		})
+}
+
+getContributors = (url, seletor) => {
+	fetch(url)
+		.then(res => res.json())
+		.then(json => {
+			let contributors = json.length
+				//console.log(commits) 
+
+			seletor.insertAdjacentHTML('beforeend', contributors);
+		})
+}
+
+button.addEventListener('click', function (event) {
+
 	boxInfoCompare.innerHTML = headProject + headCommunity + headCode;
 
 	getCompare(address1)
 	getCompare(address2)
 
-  	event.preventDefault()
+	event.preventDefault()
 })
-  
